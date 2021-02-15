406a57927803316de6c2941ae735389be47597e3
eg-vw <61621232+eg-vw@users.noreply.github.com>
2021-02-15 17:14:06 +0100

    Add files via upload

---
+++ visualize.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv('Mentions.csv')
+
+print(df)
+
+sns.set_theme(style='whitegrid')
+ax = sns.violinplot(x=df['Page Number'], y=df['Book'], inner=None, color='.8')
+ax = sns.stripplot(x=df['Page Number'], y=df['Book'], jitter=0.08, linewidth=1)
+ax.set_xlabel('Page Number', fontsize=10)
+ax.set_ylabel('Book', fontsize=6)
+plt.show()
